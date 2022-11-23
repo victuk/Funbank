183577dfe29315bf0bf06a91fead6a453ad0e78d
Victor <ukokjnr@gmail.com>
2022-11-23 22:19:18 +0100

    Funbank

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Funbank
+# ZTE Bank
 A banking simulation web application
 #### Understanding how banking app works using a simulation website application
--- js/defaults.js
+++ js/defaults.js
@@ -1,2 +1,2 @@
 // const baseUrl = "http://localhost:5000/v1/";
-const baseUrl = "https://funbank-api.onrender.com/v1/";
+const baseUrl = "https://funbank-api.onrender.com/v1/"
