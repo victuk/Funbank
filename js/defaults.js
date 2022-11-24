ea2af1f85e4546b3cd5fd96e5efd0caa3bda47e1
Victor <ukokjnr@gmail.com>
2022-11-24 18:45:13 +0100

    Reset password button added

--- js/defaults.js
+++ js/defaults.js
@@ -1,2 +1,26 @@
 // const baseUrl = "http://localhost:5000/v1/";
 const baseUrl = "https://funbank-api.onrender.com/v1/";
+
+
+
+function checkIfLoggedIn() {
+    fetch(baseUrl + "auth/checkifloggedin", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            accesstoken: localStorage.getItem("Authorization")
+        })
+    }).then(async (res) => {
+        const data = await res.json();
+        console.log(res.status);
+        if(res.status != 200) {
+            location.assign("/login.html");
+        }
+        console.log(data);
+    }).catch(error => {
+        console.log(error);
+        location.assign("/login.html");
+    });
+}
